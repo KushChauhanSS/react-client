7a388b4e4e27d837257fd6cc293d138e5e9310b1
Kush Chauhan <kush.chauhan@successive.tech>
2021-10-12 13:54:30 +0530

    feat: created a Slider component and implemented useState and useEffect hooks

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import { PropTypes } from 'prop-types';
+import { PUBLIC_IMAGE_FOLDER, DEFAULT_BANNER_IMAGE } from '../../configs/constants';
+import { getRandomNumber, getNextRoundRobin } from '../../libs/utils/math';
+import './style.css';
+
+const Slider = (props) => {
+  const {
+    altText, banners, defaultBanner, duration, height, random,
+  } = props;
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [imageSrc, setImageSrc] = useState((banners && banners.length) ? `${PUBLIC_IMAGE_FOLDER}${banners[currentIndex]}` : defaultBanner);
+  useEffect(() => {
+    let imageIndex;
+    const intervalId = setInterval(() => {
+      if (banners && banners.length) {
+        if (random) {
+          imageIndex = getRandomNumber(banners.length);
+        } else {
+          imageIndex = getNextRoundRobin(banners.length, currentIndex);
+        }
+        setImageSrc(`${PUBLIC_IMAGE_FOLDER}${banners[imageIndex]}`);
+        setCurrentIndex(imageIndex);
+      }
+    }, duration);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [currentIndex]);
+
+  return (
+    <div className="banner" style={{ height }}>
+      <img src={imageSrc} alt={altText} />
+    </div>
+  );
+};
+
+Slider.defaultProps = {
+  altText: 'Default Banner',
+  defaultBanner: DEFAULT_BANNER_IMAGE,
+  duration: 2000,
+  height: 200,
+  random: false,
+};
+
+Slider.propTypes = {
+  altText: PropTypes.string,
+  banners: PropTypes.arrayOf(PropTypes.string).isRequired,
+  defaultBanner: PropTypes.string,
+  duration: PropTypes.number,
+  height: PropTypes.number,
+  random: PropTypes.bool,
+};
+
+export default Slider;
--- src/pages/TextFieldDemo/TextFieldDemo.jsx
+++ src/pages/TextFieldDemo/TextFieldDemo.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { TextField } from '../../components';
+import { TextField, Slider } from '../../components';
+import { BANNERS } from '../../configs/constants';
 
 const TextFieldDemo = () => (
   <div>
+    <Slider banners={BANNERS} />
     <TextField heading="This is a Disabled Input" value="Disabled Input" disabled />
     <TextField heading="A Valid Input" value="Accessible" />
     <TextField heading="An Input with errors" value="101" error="Could not be greater than" />
